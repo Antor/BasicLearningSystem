aab5d4a96992579a4be0e7f8763ea1d3be5ad0bf
Andrei Tsiatsera <paladin26@narod.ru>
2019-10-04 18:16:18 +0300

    Added viewing of list of courses and list of users to admin panel.

--- basic-learning-system/src/main/java/com/gravityray/basiclearningsystem/admin/controller/ui/AdministrationController.java
+++ basic-learning-system/src/main/java/com/gravityray/basiclearningsystem/admin/controller/ui/AdministrationController.java
@@ -1,16 +1,31 @@
 package com.gravityray.basiclearningsystem.admin.controller.ui;
 
+import com.gravityray.basiclearningsystem.course.converter.CourseConverter;
+import com.gravityray.basiclearningsystem.course.service.CourseService;
 import com.gravityray.basiclearningsystem.user.controller.ui.UserInfoModelAppender;
+import com.gravityray.basiclearningsystem.user.converter.UserConverter;
+import com.gravityray.basiclearningsystem.user.model.dto.UserDto;
+import com.gravityray.basiclearningsystem.user.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
 public class AdministrationController {
 
+    private final UserService userService;
+    private final CourseService courseService;
     private final UserInfoModelAppender userInfoModelAppender;
 
-    public AdministrationController(UserInfoModelAppender userInfoModelAppender) {
+    public AdministrationController(
+            UserService userService,
+            CourseService courseService,
+            UserInfoModelAppender userInfoModelAppender) {
+        this.userService = userService;
+        this.courseService = courseService;
         this.userInfoModelAppender = userInfoModelAppender;
     }
 
@@ -27,6 +42,8 @@ public class AdministrationController {
 
         userInfoModelAppender.append(model);
 
+        model.addAttribute("courses", courseService.getCourses(false));
+
         return "administration_courses";
     }
 
@@ -34,6 +51,8 @@ public class AdministrationController {
     public String administrationUsers(Model model) {
 
         userInfoModelAppender.append(model);
+
+        model.addAttribute("users", userService.getAllUsers());
 
         return "administration_users";
     }
--- basic-learning-system/src/main/java/com/gravityray/basiclearningsystem/course/controller/ui/CourseController.java
+++ basic-learning-system/src/main/java/com/gravityray/basiclearningsystem/course/controller/ui/CourseController.java
@@ -1,44 +1,29 @@
 package com.gravityray.basiclearningsystem.course.controller.ui;
 
-import com.gravityray.basiclearningsystem.course.model.CourseEntity;
 import com.gravityray.basiclearningsystem.course.service.CourseService;
-import com.gravityray.basiclearningsystem.user.model.entity.UserEntity;
-import com.gravityray.basiclearningsystem.user.service.UserService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.gravityray.basiclearningsystem.user.controller.ui.UserInfoModelAppender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 public class CourseController {
 
-    private final UserService userService;
     private final CourseService courseService;
+    private final UserInfoModelAppender userInfoModelAppender;
 
     public CourseController(
-            UserService userService,
-            CourseService courseService) {
-        this.userService = userService;
+            CourseService courseService,
+            UserInfoModelAppender userInfoModelAppender) {
         this.courseService = courseService;
+        this.userInfoModelAppender = userInfoModelAppender;
     }
 
     @GetMapping("/course")
     public String courseList(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userInfoModelAppender.append(model);
 
-        UserEntity userEntity = userService.getUser(authentication.getName());
-        boolean authenticated = userEntity != null;
-        model.addAttribute("authenticated", authenticated);
-
-        if (authenticated) {
-            model.addAttribute("username", userEntity.getFirstName());
-        }
-
-        List<CourseEntity> courseList = courseService.getCourses(true);
-        model.addAttribute("courses", courseList);
+        model.addAttribute("courses", courseService.getCourses(true));
 
         return "course_list";
     }
