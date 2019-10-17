80f3bf6c811ffa9329d2a73235d3dbc169b2372c
Andrei Tsiatsera <paladin26@narod.ru>
2019-10-17 14:26:35 +0300

    Implemented delete lesson item page.

--- basic-learning-system/src/main/java/com/gravityray/basiclearningsystem/admin/controller/AdministrationLessonItemController.java
+++ basic-learning-system/src/main/java/com/gravityray/basiclearningsystem/admin/controller/AdministrationLessonItemController.java
@@ -1,12 +1,23 @@
 package com.gravityray.basiclearningsystem.admin.controller;
 
+import com.gravityray.basiclearningsystem.lessonitem.model.LessonItemEntity;
+import com.gravityray.basiclearningsystem.lessonitem.service.LessonItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
 
 @Controller
 public class AdministrationLessonItemController {
+
+    private final LessonItemService lessonItemService;
+
+    public AdministrationLessonItemController(LessonItemService lessonItemService) {
+        this.lessonItemService = lessonItemService;
+    }
 
     @GetMapping("/admin/course/{courseId}/unit/{unitId}/lesson/{lessonId}/item/create")
     public String lessonItemCreateGet(
@@ -39,8 +50,27 @@ public class AdministrationLessonItemController {
             @PathVariable Long lessonItemId,
             Model model) {
 
-        // TODO
+        LessonItemEntity lessonItemEntity = lessonItemService.getLessonItem(lessonItemId);
+
+        model.addAttribute("errors", Collections.emptyList());
+        model.addAttribute("course", lessonItemEntity.getLesson().getUnit().getCourse());
+        model.addAttribute("unit", lessonItemEntity.getLesson().getUnit());
+        model.addAttribute("lesson", lessonItemEntity.getLesson());
+        model.addAttribute("lessonItem", lessonItemEntity);
 
         return "administration/lesson_item_delete";
+    }
+
+    @PostMapping("/admin/course/{courseId}/unit/{unitId}/lesson/{lessonId}/item/{lessonItemId}/delete")
+    public String lessonItemDeletePost(
+            @PathVariable Long courseId,
+            @PathVariable Long unitId,
+            @PathVariable Long lessonId,
+            @PathVariable Long lessonItemId,
+            Model model) {
+
+        lessonItemService.deleteLessonItem(lessonItemId);
+
+        return String.format("redirect:/admin/course/%d/unit/%d/lesson/%d/item", courseId, unitId, lessonId);
     }
 }
